3201acaab6621816786dff53e022372d1faa78af
sb33ries <124833574+sb33ries@users.noreply.github.com>
2024-05-18 20:57:47 -0400

    updated

---
+++ script.js
@@ -0,0 +1,5 @@
+// script.js
+document.getElementById('get-started-button').addEventListener('click', function() {
+    document.getElementById('steps-section').scrollIntoView({ behavior: 'smooth' });
+  });
+  
