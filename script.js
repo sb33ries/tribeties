822843a59e9926f4435fe016085d1b9a77f3ddd8
sb33ries <124833574+sb33ries@users.noreply.github.com>
2024-05-21 16:27:02 -0400

    fix to button

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // script.js
 document.getElementById('get-started-button').addEventListener('click', function() {
-    document.getElementById('steps-section').scrollIntoView({ behavior: 'smooth' });
+    document.getElementById('steps').scrollIntoView({ behavior: 'smooth' });
   });
   
